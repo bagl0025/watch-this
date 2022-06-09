84af613cc713676a6062d0d54779da1fd6384b27
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-06-08 21:24:26 -0500

    seed sample
    adding seed sample

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+USE watch_this_db;
+
+INSERT INTO
+    watchList (name)
+VALUES
+    ('Movies'),
+    ('Shows'),
+    ('Documents');
+
+INSERT INTO
+    info (title, releasedYear, watchList_id)
+VALUES
+    ('Top Gun: Maverick', 2022, 1),
+    ('Jurassic World: Dominion', 2022, 12) ('The Bobs Burgers Movie', 60000, 3);
+
+-- INSERT INTO
+
+--     UserInfo (userName, watchEd, watchIng, willWatch,)
+
+-- VALUES
+
+--     ('JohnT', 'watchEd', 'watchIng', 'willWatch'),
+
+--     ('JannT', 'watchEd', 'watchIng', 'willWatch'),
+
+--     ('JoeT', 'watchEd', 'watchIng', 'willWatch'),
